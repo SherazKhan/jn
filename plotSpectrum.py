c363e1557b58dc2ba7cdf639929d1fc85ff8f2a4
sherazkhan <sheraz@nmr.mgh.harvard.edu>
2016-09-01 20:31:26 -0400

    combo

--- plotSpectrum.py
+++ plotSpectrum.py
@@ -7,13 +7,19 @@ Created on Sun Aug 28 13:50:33 2016
 
 import utils
 import matplotlib.pyplot as plt
+import pyimpress
+fname_mat = '/autofs/cluster/transcend/sheraz/scripts/realtimeMEG/weights.mat'
+
 fig=plt.figure('mainfig',figsize=(25,15)) 
-path='/autofs/space/megraid_research/MEG/tal/no_name/160829/'
+path='/autofs/space/megraid_research/MEG/tal/no_name/160901/'
 
+matData=pyimpress.utils.readMatFile(fname_mat)
+weights=matData['weightTop'].ravel()
+weights=1.*(weights>.5)
 
-fname_raw=path+'erm.fif'
-freqs,psds_erm=utils.plotPSD(fname_raw,tmax_crop=150.89,tmax_psd=150,
-                             nperseg=1,noverlap=90,multitapper=True,K=2)
+fname_raw=path+'erm1.fif'
+freqs,psds_erm=utils.plotPSD(fname_raw,tmax_crop=105,tmax_psd=100,
+                             nperseg=4,noverlap=90,multitapper=False,NW=2,weights=weights)
 
 #fname_raw=path+'plate.fif'
 #plotPSD(fname_raw, color=(0,1,1), label='Copper Plate')
@@ -23,27 +29,31 @@ freqs,psds_erm=utils.plotPSD(fname_raw,tmax_crop=150.89,tmax_psd=150,
 #plotPSD(fname_raw,color=(1,0,1),label='Aluminium Ring',tmax_crop=120.85,tmax_psd=120)
 
 
-fname_raw=path+'salineSphere.fif'
-freqs,psds_ss=utils.plotPSD(fname_raw,tmax_crop=150.89,tmax_psd=150,
-                            nperseg=1,noverlap=90,multitapper=True,K=2)
+fname_raw=path+'copperSulphateSphere.fif'
+freqs,psds_ss=utils.plotPSD(fname_raw,tmax_crop=105,tmax_psd=100,
+                            nperseg=4,noverlap=90,multitapper=False,NW=2,weights=weights)
 
+plt.plot(freqs,psds_ss, color=(1,0,0), linewidth=3, label='Saline Sphere')
+plt.plot(freqs,psds_erm, color=(0,0,1), linewidth=3, label='Empty Room')
+plt.ylim(0,.9e-11)
+plt.xlim(1,100)
 
 #fname_raw=path+'andyOutofMachine.fif'
 #freqs,psds_ao=utils.plotPSD(fname_raw)
 
 
-fname_raw=path+'andyINEyesClosed.fif'
-freqs,psds_ai=utils.plotPSD(fname_raw,tmax_crop=150.89,tmax_psd=150,
-                            nperseg=1,noverlap=90,multitapper=True,K=2)
+#fname_raw=path+'andyINEyesClosed.fif'
+#freqs,psds_ai=utils.plotPSD(fname_raw,tmax_crop=150.89,tmax_psd=150,
+#                            nperseg=1,noverlap=90,multitapper=True,K=2)
 
 plt.plot(freqs,psds_ss-psds_erm, color=(1,0,0), linewidth=3, label='Saline Sphere - Empty Room')
-plt.plot(freqs,psds_ai-psds_erm, color=(0,0,1), linewidth=3, label='Andy In - Empty Room')
+#plt.plot(freqs,psds_ai-psds_erm, color=(0,0,1), linewidth=3, label='Andy In - Empty Room')
 
 
 #Setting plot Parameters
 
-plt.xlim(10,500)
-plt.ylim(0,.9e-12)
+plt.xlim(1,1000)
+plt.ylim(0,.9e-11)
 plt.yticks(fontsize=20)
 plt.xticks(fontsize=20)
 plt.xlabel('Frequency (Hz)',fontsize=24)
--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ import numpy as np
 from spectrum import pmtm as pspec
 from numpy.lib import stride_tricks
 import matplotlib.pyplot as plt
+import pyimpress
 
 def subsequences(sig, frameSize, overlapFac=0.75):
     hopSize = int(frameSize - np.floor(overlapFac * frameSize))
@@ -55,10 +56,8 @@ def pmtm(y,fs,K):
 
 def get_Vertex_Channels(raw):
     selection=mne.read_selection('Vertex')
-    selection=selection[2::3]
-    selection+=['MEG 0421']
     selec=[item[0:3]+item[-4:] for item in selection]
-    picks = mne.pick_types(raw.info, meg='mag', eeg=False, eog=False,
+    picks = mne.pick_types(raw.info, meg='grad', eeg=False, eog=False,
                            stim=False, exclude='bads', selection=selec)
     return picks
 
@@ -78,41 +77,53 @@ def stmt(sig, frameSize, fs, K=4, overlapFac=0.75):
     freq=np.linspace(0,1/(2*dt),Nfft/2)
     return freq,psd
 
-def getPSD(raw,picks,tmin=10,tmax=130,nperseg=2500,noverlap=2000):
+def getPSD(raw,picks,tmin=10,tmax=130,nperseg=2500,noverlap=2000,weights=np.ones((102,))):
     start, stop = raw.time_as_index([tmin, tmax])                     
-    data, _ = raw[picks, start:(stop + 1)]
+    data = raw[picks, start:(stop + 1)][0]
+    packedData=[stft(dat,nperseg, raw.info['sfreq'],overlapFac=float(noverlap)/nperseg) for dat in data]
+    packedData=np.array(packedData)
+    freqs=packedData[0,0]
+    dat=packedData[:,1]
+    data=[item for item in dat]
+    data=np.abs(data)
+    psds_in=np.mean(np.mean(data,axis=0),axis=0)
+    #data=np.transpose(np.transpose(data)*weights)
     #freqs,psds_in =welch(np.mean(data,0),fs=raw.info['sfreq'],nperseg=nperseg,noverlap=noverlap)
-    freqs,psds_in =stft(np.mean(data,0),nperseg, raw.info['sfreq'],
-                        overlapFac=float(noverlap)/nperseg)
-    psds_in=np.mean(abs(psds_in),axis=0)
+    #freqs,psds_in =stft(np.mean(data,0),nperseg, raw.info['sfreq'],
+    #                        overlapFac=float(noverlap)/nperseg)
+    #psds_in=np.mean(abs(psds_in),axis=0)
     return freqs,psds_in
     
 
-def getMTSD(raw,picks,tmin=10,tmax=130,nperseg=2500,noverlap=2000,K=4):
+def getMTSD(raw,picks,tmin=10,tmax=130,nperseg=2500,noverlap=2000,K=4,weights=np.ones((102,))):
     start, stop = raw.time_as_index([tmin, tmax])                     
-    data, _ = raw[picks, start:(stop + 1)]
+    data = raw[picks, start:(stop + 1)][0]
+    data=(data.T+weights).T
     freqs,psds_in =stmt(np.mean(data,0),nperseg,raw.info['sfreq'],
                         overlapFac=float(noverlap)/nperseg,K=K)  
     psds_in=np.sqrt(np.mean(psds_in,axis=0))                          
     return freqs,psds_in
 
 def plotPSD(fname_raw,tmin_crop=0,tmax_crop=135,n_jobs=16,tmin_psd=10,
-            tmax_psd=130,color=(1,0,0),linewidth=3,label='',nperseg=0.5,noverlap=75,show_plot=False,multitapper=False,K=4):
+            tmax_psd=130,color=(1,0,0),linewidth=3,label='',nperseg=0.5,noverlap=75,show_plot=False,multitapper=False,NW=4,notch=False,weights=np.ones((102,))):
                 
     if  tmax_psd >  tmax_crop:
         tmax_psd = tmax_crop
     raw=mne.io.read_raw_fif(fname_raw,preload=False)
     raw.crop(tmin_crop,tmax_crop).load_data()
-    raw.notch_filter(np.arange(60, raw.info['sfreq']/2, 60), n_jobs=n_jobs)
+    if notch:
+        raw.notch_filter(np.arange(60, raw.info['sfreq']/2, 60), n_jobs=n_jobs)
     nperseg=int(nperseg*raw.info['sfreq'])
     noverlap=int((noverlap/100.)*nperseg)
     picks = get_Vertex_Channels(raw)
+    #picks = mne.pick_types(raw.info, meg='mag', eeg=False, eog=False,
+    #                    stim=False, exclude='bads')
     if multitapper:
         freqs,psds=getMTSD(raw, picks, tmin=tmin_psd, 
-                                 tmax=tmax_psd, nperseg=nperseg, noverlap=noverlap,K=K)
+                                 tmax=tmax_psd, nperseg=nperseg, noverlap=noverlap,K=NW,weights=weights)
     else:
         freqs,psds=getPSD(raw, picks, tmin=tmin_psd,
-                                tmax=tmax_psd, nperseg=nperseg, noverlap=noverlap)
+                                tmax=tmax_psd, nperseg=nperseg, noverlap=noverlap,weights=weights)
     if show_plot:
         handle=plt.plot(freqs,psds, color=color, linewidth=linewidth, label=label)
         return freqs,psds,handle
